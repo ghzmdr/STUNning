102762289041a959133a36929c642f73e8e81a12
Vincent <ghzmdr@gmail.com>
2015-08-11 20:41:07 +0200

    more logging

--- index.js
+++ index.js
@@ -71,9 +71,10 @@ stunning.prototype.registerServer = function(info) {
     
     if (!this.server) {
         this.server = new Endpoint(info.address, info.port)
-        console.log('\n\nGOT SERVER\n', this.server.toString())
+        console.log('\n\n[++] ===== GOT SERVER\n', this.server.toString())
         this.server.send(this.socket, this.clients.length > 0 ? JSON.stringify(this.clients) : OP_CODES.NO_CLIENTS_CONNECTED)
         
+        console.log(this.server ? "[ ] INTRODUCING HIM THE CLIENTS")
         for (var i = 0; i < this.clients.length; ++i) {
             this.clients[i].send(this.socket, JSON.stringify(this.server))
         }
@@ -93,10 +94,12 @@ stunning.prototype.registerClient = function(info) {
          
     var client = new Endpoint(info.address, info.port)    
     this.clients.push(client)    
-    console.log('\n\nGOT CLIENT\n', client.toString())
+    console.log('\n\n[++] ===== GOT CLIENT\n', client.toString())
+
 
     var connectionInfo = this.server ? JSON.stringify(this.server) : OP_CODES.SERVER_NOT_CONNECTED
 
+    console.log(this.server ? "[ ] INTRODUCING HIM THE SERVER: \n" + this.server + toString)
     client.send(this.socket, connectionInfo)
     
     if (this.server) 
